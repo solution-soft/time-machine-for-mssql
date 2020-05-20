16898238e812c403da47925122b790b9c3246766
Kenneth Zhao <kzhao@solution-soft.com>
2020-05-20 15:54:47 -0700

    make sure mssql data dir and subdirs have the correct ownership and permission

--- build/etc/cont-init.d/02-mssql-validates.sh
+++ build/etc/cont-init.d/02-mssql-validates.sh
@@ -2,11 +2,18 @@
 
 set -e
 
+MSSQL_HOME=/var/opt/mssql
+MSSQL_UID=${MSSQL_UID:-10001}
+MSSQL_GID=${MSSQL_GID:-0}
+
 # Make sure there is enough RAM
 avail=`free -g -t | grep Mem | awk '{ print $7}'`
 if [ $avail -lt 2 ]; then
   echo "[Error] There is not enough free mem to run Microsoft MSSQL server!"
   exit 1
 fi
+
+# correct permission setting
+chown -R mssql $MSSQL_HOME
 
 exit 0
